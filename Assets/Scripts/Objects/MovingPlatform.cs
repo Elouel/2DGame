b49bb430c72edf1aec86cf55ed5f2406503e477d
Nikolay Nikolov <nikolay.nikolov@amexis.int>
2016-05-14 16:30:00 +0300

    save

---
+++ Assets/Scripts/Objects/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public GameObject objectToMove;
+    public float moveSpeed;
+    public Transform[] points;
+    public int pointSelection;
+    public bool loop = true;
+
+    private Transform currentPoint;
+    // Use this for initialization
+    void Start()
+    {
+        currentPoint = points[pointSelection];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(objectToMove.transform.position);
+        objectToMove.transform.position = Vector2.MoveTowards(objectToMove.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+
+        if (objectToMove.transform.position == currentPoint.position)
+        {
+            pointSelection++;
+            if (loop && pointSelection == points.Length)
+            {
+                pointSelection = 0;
+            }
+            currentPoint = points[pointSelection];
+        }
+    }
+}

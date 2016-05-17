d8fe8b80576bc57744e7c7636a8c7062bd83781c
Nikolay Nikolov <nikolay.nikolov@amexis.int>
2016-05-17 12:52:01 +0300

    finished platform + removed level1 from parallax effect

--- Assets/Scripts/Objects/MovingPlatform.cs
+++ Assets/Scripts/Objects/MovingPlatform.cs
@@ -11,16 +11,26 @@ public class MovingPlatform : MonoBehaviour
     public bool loop = true;
 
     private Transform currentPoint;
+    private BackgroundParallax parallaxScript;
+    private Transform cam;
+    private Vector3 previousCamPos;
+
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
     // Use this for initialization
     void Start()
     {
         currentPoint = points[pointSelection];
+        parallaxScript = (BackgroundParallax)FindObjectOfType(typeof(BackgroundParallax));
+        previousCamPos = cam.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(objectToMove.transform.position);
         objectToMove.transform.position = Vector2.MoveTowards(objectToMove.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
 
         if (objectToMove.transform.position == currentPoint.position)
